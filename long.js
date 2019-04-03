243f08d021568795bb52809e3f8dc6b25b6fe964
qt7288 <jt3898@sina.com>
2019-04-03 21:55:11 +0800

    啊飒飒

--- long.js
+++ long.js
@@ -16,9 +16,9 @@ const cors = require("cors");
 var server=express();
 
 server.use(cors({
-	origin:["http://172.242.3.181:8080",
-	"http://localhost:8080",
-	"http://172.242.3.181:7777"],
+	origin:["http://172.242.3.181:5050",
+	// "http://localhost:8080",
+	"http://172.242.3.181:5050"],
 	credentials:true
   }));
 server.listen(5050);
