c2610717617fb498cb3934c80341b68a9e9c103b
qt7288 <jt3898@sina.com>
2019-04-03 21:27:48 +0800

    000

--- long.js
+++ long.js
@@ -3,9 +3,9 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 //引入user文件
-const userRouter=require('./routes/long_user.js');
+// const userRouter=require('./routes/long_user.js');
 //const productRouter=require('./routes/product.js');
-const demo=require('./routes/demo.js');
+// const demo=require('./routes/demo.js');
 const myPro=require('./routes/myPro.js');
 const rootR=require('./routes/rootR.js');
 
@@ -23,8 +23,8 @@ server.use(cors({
   }));
 server.listen(5050);
 //托管静态文件
-server.use(express.static('./public'));
-server.use(express.static('./ajax'));
+// server.use(express.static('./public'));
+// server.use(express.static('./ajax'));
 server.use(express.static('./myPro'));
 server.use(express.static('./rootR'));
 
@@ -33,8 +33,8 @@ server.use(bodyParser.urlencoded({
 }));
 //use路由器管理路由
 //把用户路由器挂载到/user下，访问形式/user/detail
-server.use('/long_user',userRouter);
+// server.use('/long_user',userRouter);
 //server.use('/product',productRouter);
-server.use('/demo',demo);
+// server.use('/demo',demo);
 server.use('/myPro',myPro);
 server.use('/rootR',rootR);
